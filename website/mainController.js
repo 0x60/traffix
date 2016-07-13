d83790eaff4d4b9725fde4988c56551301750d2d
Albert Tung <atung3@stanford.edu>
2016-07-13 00:07:43 -0700

    Icon changes, finished hover, and some basic footer setup

--- website/mainController.js
+++ website/mainController.js
@@ -5,4 +5,12 @@ var traffixApp = angular.module('traffixApp', []);
 traffixApp.controller('MainController', ['$scope', '$rootScope', '$http',
 function ($scope, $rootScope, $http) {
 
+	$scope.icons = [
+		{"Alert":["images/warningImage.png", "images/warningImageHover.png"]}, 
+		{"Potential Hotspots": ["images/potentialHotspots.png", "images/potentialHotspotsHover.png"]},
+		{"Settings": ["images/settings.png", "images/settingsHover.png"]}
+	];
+
+
+
 }]);
