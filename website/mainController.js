d463b669f98e87fc1807eb8f479a11c48fdb32bb
Albert Tung <atung3@stanford.edu>
2016-07-12 21:51:01 -0700

    Website Angular Integration (with logos)

---
+++ website/mainController.js
@@ -0,0 +1,8 @@
+// Traffix App
+
+var traffixApp = angular.module('traffixApp', []);
+
+traffixApp.controller('MainController', ['$scope', '$rootScope', '$http',
+function ($scope, $rootScope, $http) {
+
+}]);
