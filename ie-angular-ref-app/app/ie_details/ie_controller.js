a33e02496a7e77a40ebe75ced3000a15d69f8468
Alexander Chang <alexanderchang@me.com>
2016-07-30 21:13:59 -0700

    Minor Fixes

--- ie-angular-ref-app/app/ie_details/ie_controller.js
+++ ie-angular-ref-app/app/ie_details/ie_controller.js
@@ -149,6 +149,7 @@ app.controller('IEServiceCtrl', ['$scope','CurrentServices',function($scope, Cur
               publicSafetyData.imageToDisplay = data;
             }, function() {
               if(!publicSafetyData.imageToDisplay) {
+                console.log( "no image to display" );
                 publicSafetyData.imageToDisplay = 'images/parking_location.png';
               }
             });
